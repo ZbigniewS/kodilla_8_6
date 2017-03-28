951c798a01a509655e6be72d9e477068f179d2d4
ZbigniewS <zbigniewsitarz@gmail.com>
2017-03-28 23:04:43 +0200

    modyfikacja pliku js. usunięcie błedów dodanie pobierania danych i wyświetlenia alertu z wynikiem.

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,18 @@
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(5, 15);
 var triangle3Area = getTriangleArea(25, 15);
-// var a = prompt('Podaj wartość a');
-// var h = prompt('Podaj wartość h');
+var a = prompt('Podaj wartość a');
+var h = prompt('Podaj wartość h');
+var triangleArea = getTriangleArea (a, h);
 
 function getTriangleArea(a, h) {
     if ((a > 0) && (h > 0)) {
         //obie wartości dodatnie wykonanie funkcji
-        triangleArea = a * h / 2;
+        return a * h / 2;
     } else {
         // jedna z wartości nie spełnia warunku liczby dodatniej.
-        console.log('Nieprawidłowe dane')
-    }
-    return triangleArea;
-    
+        return 'Nieprawidłowe dane'
+    }  
 }
-console.log(getTriangleArea(5, 6));
-console.log(getTriangleArea(10, 15));
-console.log(getTriangleArea(5, 15));
-console.log(getTriangleArea(25, 15));
-console.log('Wynik - pole trójkąta dla podanych danych wynosi ' + triangleArea);
-//console.log('Wynik - pole trójkąta dla podanych danych wynosi ' + triangleArea);
+alert ('Wynik - pole trójkąta dla podstawy równej: ' + a  + ' i wysokości równej: '+ h + ' wynosi ' + triangleArea);
+console.log('Wynik - pole trójkąta dla podstawy równej: ' + a  + ' i wysokości równej: '+ h + ' wynosi ' + triangleArea);
